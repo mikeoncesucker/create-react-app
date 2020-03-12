5a6042ebd3d63af378fa9e2b64c22c41d64dcc98
pengxiaodong <704700518@qq.com>
2020-03-12 09:31:50 +0800

    修改适应css module

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import styles from './App.module.less';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles['App']}>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+export default App;
